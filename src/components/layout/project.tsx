93394988b606074029b0f547e20795d1768807ff
rakaprdana <raka.pradana1103@gmail.com>
2025-04-02 12:04:40 +0700

    add: new project list

--- src/components/layout/project.tsx
+++ src/components/layout/project.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import SeviGoLP from "../../../public/image/SeviGo-LP.png";
 import MiniLibLP from "../../../public/image/MiniLib-LP.png";
+import Warehouse from "../../../public/image/Warehouse-Management.png";
 import { useState } from "react";
 
 const ProjectSection = ({ id }: { id: string }) => {
@@ -17,6 +18,7 @@ const ProjectSection = ({ id }: { id: string }) => {
       alt: "Project",
       descriptions:
         "SeviGo is a website that my team and I developed to address issues related to complaint reporting. In several regions of Indonesia, there are still no services that facilitate the submission of complaints. To solve this problem, my team and I decided to create a platform that offers an easy-to-use solution. Here are the key features of SeviGo: Multi-platform Support: SeviGo is designed to be accessible on various devices, including laptops and smartphones, ensuring that users can access the website from any platform. Status Tracking: Users can track the progress of their submitted complaints. The status categories include Accepted, Processing, Finished, and Rejected.",
+      link: "https://sevi-go-three.vercel.app/",
     },
     {
       src: MiniLibLP,
@@ -24,6 +26,15 @@ const ProjectSection = ({ id }: { id: string }) => {
       alt: "Project",
       descriptions:
         "MiniLib is an innovative website that my team and I developed to fulfill the specific needs of our academic program by simplifying the search for published student journals and internship reports. For the frontend, we employed React and TailwindCSS to create a responsive and intuitive user interface. On the backend, we utilized pure PHP to ensure robust and efficient server-side operations. This project involved meticulous planning and extensive collaboration to deliver a solution that meets high standards of performance, usability, and reliability. MiniLib significantly enhances the accessibility and discoverability of academic documents, representing a substantial contribution to our academic community.",
+    },
+    {
+      src: Warehouse,
+      title:
+        "Warehouse Management: A Management Website for manage youre warehouse",
+      alt: "Project",
+      descriptions:
+        "Warehouse Management is a web application built to optimize warehouse operations. The project leverages React, TypeScript, and TailwindCSS for the frontend. For the backend, it uses Express with TypeScript for efficient server-side functionality, while MongoDB serves as the database solution for secure and scalable data storage.",
+      link: "https://warehouse-inventory-chi.vercel.app/",
     },
   ];
   return (
@@ -78,9 +89,15 @@ const ProjectSection = ({ id }: { id: string }) => {
                     : "max-h-0 opacity-0"
                 }`}
               >
-                <p className="text-sm mt-4 text-gray-600 leading-relaxed">
+                <p className="text-sm my-4 text-gray-600 leading-relaxed">
                   {item.descriptions}
                 </p>
+                <a
+                  href={item.link}
+                  className="text-sm mt-4 text-blue-600 leading-relaxed font-bold"
+                >
+                  {item.link}
+                </a>
               </div>
             </div>
           </div>
